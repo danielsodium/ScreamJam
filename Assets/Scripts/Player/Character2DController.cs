0e1cf81855be40a20ae67da4272deb7bd50562d3
Bob Dylan <lym20041026@gmail.com>
2021-10-16 15:18:04 -0700

    Updated Movement

--- Assets/Scripts/Player/Character2DController.cs
+++ Assets/Scripts/Player/Character2DController.cs
@@ -10,10 +10,12 @@ public class Character2DController : MonoBehaviour
     [SerializeField] private float step_wait = 0.5f;
     public LayerMask player_mask;
     public Transform ground_check_transform;
-    public float check_radius = 0.3f;
+    public float check_radius = 0.1f;
 
     public GameObject left_leg;
     public GameObject right_leg;
+    public GameObject left_arm;
+    public GameObject right_arm;
     Rigidbody2D left_legRB;
     Rigidbody2D right_legRB;
 
@@ -22,31 +24,25 @@ public class Character2DController : MonoBehaviour
     private float _movement;
     private bool _is_space_down;
     private bool _facing_right = true;
-
-
+/*    private Vector3 delta;
+    private Vector3 previous_position;
+    private Transform[] children_transform;
+*/
     private void Start()
     {
         left_legRB = left_leg.GetComponent<Rigidbody2D>();
         right_legRB = right_leg.GetComponent<Rigidbody2D>();
         Vector3 transform_vector = transform.localScale;
         _facing_right = (transform_vector.x > 0);
-
-    }
+/*        previous_position = body_collider.bounds.center;
+        children_transform = GetComponentsInChildren<Transform>();
+*/    }
     private void Update()
     {
         _movement = Input.GetAxis("Horizontal");
         if (Input.GetButtonDown("Jump"))
         {
             _is_space_down = true;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (_is_space_down && isGrounded())
-        {
-            jump();
-            _is_space_down = false;
         }
         if (_movement == 0)
         {
@@ -76,6 +72,24 @@ public class Character2DController : MonoBehaviour
 
     }
 
+    private void FixedUpdate()
+    {
+        if (_is_space_down && isGrounded())
+        {
+            jump();
+            _is_space_down = false;
+        }
+        /*delta = body_collider.bounds.center - previous_position;
+        this.gameObject.transform.position += delta;
+        foreach (Transform i in children_transform)
+        {
+            i.position -= delta;
+        }
+        previous_position = body_collider.bounds.center;*/
+
+    }
+
+    
     private bool isGrounded()
     {
         if (Physics2D.OverlapCircle(ground_check_transform.position, check_radius, player_mask))
@@ -91,6 +105,8 @@ public class Character2DController : MonoBehaviour
         Vector3 transform_scale = transform.localScale;
         transform_scale.x *= -1;
         transform.localScale = transform_scale;
+        left_arm.GetComponent<Balance>().targetRotation *= -1;
+        right_arm.GetComponent<Balance>().targetRotation *= -1;
     }
 
     private void jump()

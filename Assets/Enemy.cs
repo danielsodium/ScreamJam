4de23993542a2f64a88c8ee2ead14c431311a4ec
McButtChuggins <irontakeout@gmail.com>
2021-10-23 15:15:38 -0700

    Everything fixed

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int health = 100;
+    public int damage = 40;
     public GameObject deathEffect;
     public void TakeDamage (int damage){
         health -= damage;
@@ -17,5 +18,11 @@ public class Enemy : MonoBehaviour
         Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
-   
+   void OnTriggerEnter2D (Collider2D hitInfo){
+        Character_Health character_Health = hitInfo.GetComponent<Character_Health>();
+        if(character_Health != null){
+            character_Health.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
 }

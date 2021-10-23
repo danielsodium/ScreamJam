448818a585a97883eec031f39d78ce999661acbc
McButtChuggins <irontakeout@gmail.com>
2021-10-23 14:54:46 -0700

    Revert "enemy health/dmg/player health"
    This reverts commit 97f42fc0787cc3ba536218adeced68f9d6079a72.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,26 +5,17 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int health = 100;
-    public int damage = 40;
     public GameObject deathEffect;
-    public void TakeDamage (int damage)
-    {
+    public void TakeDamage (int damage){
         health -= damage;
-        if (health <=0)
-        {
+        if (health <=0){
             Die();
         }
     }
 
-    void Die ()
-    {
+    void Die (){
         Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
-    void OnTriggerEnter2D (Collider2D hitInfo){
-        Character_Health character_Health = hitInfo.GetComponent<Character_Health>();
-        if(character_Health != null){
-            character_Health.TakeDamage(damage);
-        }
-    }
+   
 }
